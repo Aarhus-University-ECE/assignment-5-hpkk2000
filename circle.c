0693610de4ea6421caade5a65bc13380288ec9d0
Hans Peter Kronholm <112431206+hpkk2000@users.noreply.github.com>
2022-10-06 23:00:01 +0200

    Update circle.c

--- circle.c
+++ circle.c
@@ -5,6 +5,7 @@
 
 void fiveCircles(circle c[]) {
 	for (int i = 0; i < 5; i++){
+//Loop over fives circles i=1->5 and set their coordinate and radius accordingly.
 	c[i].p.x = i;
 	c[i].p.y = i;
 	c[i].r = i;
@@ -13,17 +14,18 @@ void fiveCircles(circle c[]) {
 }
 
 bool circleIsValid(const circle * c) {
-if(c->r > 0)
-{
-	return true;
+	//The circle is valid if it's radius is above 0.
+if(c->r > 0){
+	return 1;
 }
-	else
-{
-	return false;
+else{
+  return 0;
 }
 }
+
 
 void translate(circle* c, const point* p) {
 c->p.x += p->x;
 c->p.y += p->y;
 }
+
